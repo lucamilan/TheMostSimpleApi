cf7d0dcbe5abd45e910c9d1ffdaa8c54fe12709f
Luca Milan <luca_milan@hotmail.com>
2023-08-31 12:05:51 +0200

    My Api

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "The Most Simple API", Description = "The most Simple API", Version = "v1" });
+});
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+   c.SwaggerEndpoint("/swagger/v1/swagger.json", "Simple API V1");
+});
+app.MapGet("/", () => "The Ultimate Question of Life, the Universe, and Everything is: 42");
+
+app.Run();
